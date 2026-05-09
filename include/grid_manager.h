7e52edd85c9affe0e85249063b98ce51c0045470
jmitch7574 <j.mitchell7574@gmail.com>
2026-05-09 07:01:11 +0100

    refactor: Grid related functions now belong to grid_manager

---
+++ include/grid_manager.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define CELL_SIZE 50
+#define CELL_COUNT 32
+
+#define TOTAL_SIZE (CELL_SIZE * CELL_COUNT)
+
+#define START_X (960 - TOTAL_SIZE / 2.0f)
+#define START_Y (540 - TOTAL_SIZE / 2.0f)
+
+class GridManager {
+  static void DrawLevelGrid();
+  static Vector2 GridPositionToWorld(Vector2 GridPos);
+};
+
---
+++ src/engine/grid_manager.cpp
@@ -0,0 +1,22 @@
+#include "grid_manager.h"
+#include "raylib-cpp.hpp"
+
+void GridManager::DrawLevelGrid() {
+  
+  const float endX = START_X + TOTAL_SIZE;
+  const float endY = START_Y + TOTAL_SIZE;
+  const Color gridColor = Fade(WHITE, 0.3f);
+
+  for (int i = 0; i <= CELL_COUNT; i++) {
+    float offset = i * CELL_SIZE;
+    DrawLineV(Vector2{START_X + offset, START_Y}, Vector2{START_X + offset, endY},
+              gridColor);
+    DrawLineV(Vector2{START_X, START_Y + offset}, Vector2{endX, START_Y + offset},
+              gridColor);
+  }
+}
+
+Vector2 GridManager::GridPositionToWorld(Vector2 GridPos)
+{
+  return Vector2Add(Vector2(START_X, START_Y), Vector2Scale(GridPos, CELL_SIZE));
+}
