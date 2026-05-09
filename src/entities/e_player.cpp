b7530a351703e1d27029d2c24e449c1d31ae8932
jmitch7574 <j.mitchell7574@gmail.com>
2026-05-09 08:10:21 +0100

    feat: Player Trail and Diagonal Movement

--- src/entities/e_player.cpp
+++ src/entities/e_player.cpp
@@ -1,6 +1,14 @@
 #include "entity.h"
 #include "grid_manager.h"
 
+struct PlayerTrail {
+  Vector2 pos;
+  int opacity;
+  float radius;
+};
+
+PlayerTrail trail[120] = {0};
+
 Player::Player(raylib::Vector2 startPos) : Entity(startPos) {
   gridPosition = startPos;
   position = GridManager::GridPositionToWorld(gridPosition);
@@ -13,19 +21,41 @@ void Player::Update() {
   position = Vector2Lerp(position, GridManager::GridPositionToWorld(gridPosition), lerpSpeed);
   currentMoveCooldown -= GetFrameTime();
 
+  Vector2 gridPositionLastFrame = gridPosition;
   if (IsKeyDown(KEY_LEFT)) TryMove(Vector2(-1, 0));
   if (IsKeyDown(KEY_RIGHT)) TryMove(Vector2(1, 0));
   if (IsKeyDown(KEY_UP)) TryMove(Vector2(0, -1));
   if (IsKeyDown(KEY_DOWN)) TryMove(Vector2(0, 1));
+  
+  if (!Vector2Equals(gridPosition, gridPositionLastFrame)) 
+    currentMoveCooldown = moveCooldown;
+
+  for (int i = 119; i > 0; i--) {
+    trail[i] = trail[i - 1];
+    trail[i].radius -= 0.5f;
+    trail[i].opacity -= 25;
+
+    if (Vector2Equals(trail[i].pos, position)) {
+      trail[i].opacity = 0;
+    }
+  }
+
+  trail[0] = {position, 255, CELL_SIZE / 4.0f};
 }
 
 void Player::Draw() {
+  for (int i = 119; i > 0; i--) {
+    if (trail[i].opacity < 0) continue;
+
+    int trail_radius = (CELL_SIZE / 2.0f) - ((255 - trail[i].opacity) / 10);
+    DrawCircleV(trail[i].pos, CELL_SIZE / 2.0f, Color{ 50, 120, 160, (unsigned char) trail[i].opacity });
+  }
+
   DrawCircleV(position, CELL_SIZE / 2.0f, SKYBLUE);
 }
 
 void Player::TryMove(Vector2 dir) {
   if (currentMoveCooldown < 0) {
-    currentMoveCooldown = moveCooldown;
     gridPosition = Vector2Add(gridPosition, dir);
   }
 }
