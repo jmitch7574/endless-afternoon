9fadded831ed3a03093b0d205d7091e17f3a7164
jmitch7574 <j.mitchell7574@gmail.com>
2026-05-10 00:23:18 +0100

    Update s_3_playmode.cpp

--- src/scenes/s_3_playmode.cpp
+++ src/scenes/s_3_playmode.cpp
@@ -33,7 +33,7 @@ PlayMode *playScene;
 
 PlayMode::PlayMode()
 	: player(Vector2{10, 10}), enemy(Vector2{5, 5}), minuteHand(Vector2{960, 540}, -90.0f, 440.0f, 8.0f, WHITE),
-	  hourHand(Vector2{960, 540}, -90.0f, 280.0f, 12.0f, WHITE)
+	  hourHand(Vector2{960, 540}, -90.0f, 280.0f, 12.0f, WHITE), dangerEffects()
 {
 	playScene = this;
 }
@@ -122,6 +122,8 @@ void PlayMode::Update()
 	{
 		BeginGameOver();
 	}
+	
+	dangerEffects.Update();
 }
 void PlayMode::Draw()
 {
@@ -145,7 +147,7 @@ void PlayMode::Draw()
 		enemy.Draw();
 	}
 	DrawEnemyHealthBar();
-
+	dangerEffects.Draw();
 #ifndef NDEBUG
 	DrawText(TextFormat("Player Pos: %f, %f", player.gridPosition.x, player.gridPosition.y), 20, 20, 20, WHITE);
 	DrawText(TextFormat("Player Health: %f / %i", player.GetHealth(), 100), 20, 50, 20, WHITE);
