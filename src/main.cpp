d473ff744d0cd341231f75a3d313f7bd0874c314
jmitch7574 <j.mitchell7574@gmail.com>
2026-05-09 11:53:28 +0100

    feat: renderer rubbish

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <emscripten/emscripten.h>
 #endif
 #include "scene_manager.h"
+#include "renderer.h"
 
 int renderTextureWidth = 1920;
 int renderTextureHeight = 1080;
@@ -15,9 +16,8 @@ void UpdateDrawFrame(void); // Update and Draw one frame
 int main() {
   raylib::Window window(1920, 1080, "raylib-cpp [core] example - basic window");
 
-  ToggleFullscreen();
-  target = LoadRenderTexture(renderTextureWidth, renderTextureHeight);
-  DisableCursor();
+  Renderer::InitRenderTexture();
+  //DisableCursor();
 
 #if defined(PLATFORM_WEB)
   emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
@@ -27,13 +27,17 @@ int main() {
 
   g_SceneManager.SetScene(std::make_unique<MainMenu>());
 
+  SetWindowState(FLAG_WINDOW_UNDECORATED);
+  SetWindowSize(GetMonitorWidth(0), GetMonitorHeight(0));
+  SetWindowPosition(0, 0);
+
   while (!window.ShouldClose())
   {
 
     g_SceneManager.Update();
 
     // Render Texture - Actual Game
-    BeginTextureMode(target);
+    BeginTextureMode(rt_RenderTexture);
 
     g_SceneManager.Draw();
 
@@ -42,28 +46,11 @@ int main() {
     // Flip Render Texture to Any Resolution
     BeginDrawing();
 
-    ClearBackground(BLACK);
+    Renderer::BlipRenderTexture();
 
-    float scaleX = (float)GetScreenWidth() / renderTextureWidth;
-    float scaleY = (float)GetScreenHeight() / renderTextureHeight;
-    float scale = (scaleX < scaleY) ? scaleX : scaleY;
-
-    int scaledWidth = (int)(renderTextureWidth * scale);
-    int scaledHeight = (int)(renderTextureHeight * scale);
-
-    int offsetX = (GetScreenWidth() - scaledWidth) / 2;
-    int offsetY = (GetScreenHeight() - scaledHeight) / 2;
-
-    DrawTexturePro(target.texture,
-                   Rectangle{0, 0, (float)target.texture.width,
-                             (float)-target.texture
-                                 .height}, // Source rectangle (flip vertically)
-                   Rectangle{(float)offsetX, (float)offsetY, (float)scaledWidth,
-                             (float)scaledHeight}, // Destination rectangle
-                   Vector2{0, 0},                  // Origin at top-left
-                   0.0f,                           // No rotation
-                   WHITE                           // Tint
-    );
+#ifdef ARCADEMIA
+    DrawText("Arcademia Edition", 20, 20, 32, WHITE);
+#endif
 
     EndDrawing();
   }
