edb3bced47495deb8ecf845515e4b89c4805d4e4
jmitch7574 <j.mitchell7574@gmail.com>
2026-05-10 01:12:15 +0100

    feat: hitflash on player

--- include/danger_effects.h
+++ include/danger_effects.h
@@ -5,4 +5,12 @@ public:
   DangerEffects();
   void Update();
   void Draw();
+  static void DisplayHurt();
+  static DangerEffects* singleton;
+  
+  float targetOpacity;
+  float opacity;
+
+  float targetVolume;
+  float volume = 0;
 };
